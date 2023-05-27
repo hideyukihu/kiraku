cbf99841faea585265212c6eba63c8089e1b89ec
hide <majihaik@gmail.com>
2023-05-27 18:10:32 +0900

    create user

---
+++ laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ]);
+        return response()->json("User Create!!");
+    }
+}
