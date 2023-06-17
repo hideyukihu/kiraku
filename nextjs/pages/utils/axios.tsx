a7375252ad702bf95e59893f343a1bb4c734a059
hideyukihu <majihaik@gmail.com>
2023-06-17 19:43:18 +0900

    CORS設定

--- nextjs/pages/utils/axios.tsx
+++ nextjs/pages/utils/axios.tsx
@@ -6,6 +6,8 @@ function createAxiosInstance() {
     withCredentials: true,
   });
 
+  Axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost:3000';
+
   return Axios;
 }
 
