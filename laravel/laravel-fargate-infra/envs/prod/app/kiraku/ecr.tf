25a617db55a9aadcc8dc2933263f9ac04ba9c9a0
hide <majihaik@gmail.com>
2023-07-06 09:22:01 +0900

    ECR作成分

--- laravel/laravel-fargate-infra/envs/prod/app/kiraku/ecr.tf
+++ laravel/laravel-fargate-infra/envs/prod/app/kiraku/ecr.tf
@@ -5,3 +5,32 @@ resource "aws_ecr_repository" "nginx" {
     Name = "ecs-prod-kiraku-nginx"
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "nginx" {
+  policy = jsonencode(
+    {
+      "rules" : [
+        {
+          "rulePriority" : 1,
+          "description" : "Holdonly10images,expireallothers",
+          "selection" : { "tagStatus" : "any", "countType" : "imageCountMoreThan", "countNumber" : 10 },
+          "action" : { "type" : "expire"
+      } }]
+    }
+  )
+  repository = aws_ecr_repository.nginx.name
+}
+
+module "nginx" {
+  source = "../../../../modules/ecr"
+  name   = "${local.name_prefix}-${local.service_name}-nginx"
+}
+
+module "php" {
+    source = "../../../../modules/ecr"
+
+    name = "${local.name_prefix}-${local.service_name}-php"
+}
+
+
+
